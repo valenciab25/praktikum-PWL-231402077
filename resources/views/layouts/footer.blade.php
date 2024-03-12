cf5e70b2fe8ec0fe04892cb3f2d26651bf30ab9b
valenciab25 <palency123@gmail.com>
2024-03-12 09:01:25 +0700

    update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content fixed bottom-0 ">  
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Alwin Liufandy</p>
+    </aside>
+  </footer>
